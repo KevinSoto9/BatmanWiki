6a34c16e15ff3f90f85d675e029fa8897f9604ae
kevin <KevinS9sotob@hotmail.com>
2024-10-03 17:48:11 +0200

    initial concepts

---
+++ src/components/types/Concept.ts
@@ -0,0 +1,12 @@
+interface Concept {
+    id: number;
+    name: string;
+    description: string;
+    related_characters: [];
+    related_locations: [];
+    first_appearance: string;
+    creator: string;
+    type: string;
+    }
+
+export default Concept;
